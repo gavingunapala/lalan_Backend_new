3ecedc453a5bbde1d46edb2856fe056881489db5
Poojanifernando <IT20156374@my.sliit.lk>
2022-12-29 12:50:37 +0530

    admin control rest apis

--- src/main/java/com/example/Lalan/Services/AdminService.java
+++ src/main/java/com/example/Lalan/Services/AdminService.java
@@ -1,18 +1,25 @@
 package com.example.Lalan.Services;
 
+import com.example.Lalan.DTO.AdminDTO;
+import com.example.Lalan.DTO.BatchDTO;
+import com.example.Lalan.Entity.AdminEntity;
+import com.example.Lalan.Entity.BatchEntity;
 import com.example.Lalan.Repos.AdminRepo;
 import com.example.Lalan.DTO.LineRegistrationDTO;
 import com.example.Lalan.DTO.ResponseDTO;
 import com.example.Lalan.Entity.LineRegistrationEntity;
 
+import com.example.Lalan.Util.VarList;
 import com.sun.corba.se.impl.oa.poa.ActiveObjectMap;
 import org.hibernate.mapping.Value;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -57,5 +64,59 @@ public class AdminService {
     public List<Map<ActiveObjectMap.Key, Value>> GetValueByDevice(String Jobid, String batchid, String LineId,
             int pOrder, String MachineId, String DeviceId, String PerId, Date Date11) {
         return adminRepo.GetValueByDevice(Jobid, batchid, LineId, pOrder, MachineId, DeviceId, PerId, Date11);
+    }
+
+    public String saveAdmin(AdminDTO adminDTO) {
+        //if the batch is already there in the db return duplicate massage
+        if (adminRepo.existsById(adminDTO.getAdminid_ad())) {
+            return VarList.RSP_DUPLICATED;
+
+        } else {
+            //if not save a new batch to the db
+            adminRepo.save(modelMapper.map(adminDTO, AdminEntity.class));
+            return VarList.RSP_SUCCESS;
+        }
+    }
+
+    public String updateAdmin(AdminDTO adminDTO) {
+        //should be checked the batch is already exit
+        if (adminRepo.existsById(adminDTO.getAdminid_ad())) {
+            //if the batch is already there update the data
+            adminRepo.save(modelMapper.map(adminDTO, AdminEntity.class));
+            return VarList.RSP_SUCCESS;
+        } else {
+            // if the batch is not already there return error
+            return VarList.RSP_NO_DATA_FOUND;
+        }
+    }
+
+    public List<AdminDTO> getAllAdmins() {
+        //find the all data in db using findAll() and return as  list
+        List<AdminEntity> adminList = adminRepo.findAll();
+        return modelMapper.map(adminList, new TypeToken<ArrayList<AdminDTO>>() {
+
+        }.getType());
+    }
+
+    public AdminDTO searchAdmin(String adminid_ad){
+        if (adminRepo.existsById(adminid_ad)){
+            AdminEntity admin = adminRepo.findById(adminid_ad).orElse(null);
+            return modelMapper.map(admin,AdminDTO.class);
+        }else {
+            return null;
+        }
+    }
+
+    public String deleteAdmin(String adminid_ad){
+        //checked , the batch already in there
+        if (adminRepo.existsById(adminid_ad)){
+            //if the batch is exit delete the batch by batch id
+            adminRepo.deleteById(adminid_ad);
+            //and return a success massage
+            return VarList.RSP_SUCCESS;
+        }else {
+            //if not return an error message
+            return VarList.RSP_NO_DATA_FOUND;
+        }
     }
 }
